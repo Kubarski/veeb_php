2d6d0889f2ca8c7320a989426fe529232ff1fe5d
Kubarski <ki.tax@hotmail.com>
2019-02-13 14:21:49 +0200

    muutujate loomine

---
+++ PHP_alused/Variables/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katriin
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+//muutujate loomine
+$eesnimi = 'Katriin';
+$perenimi = 'Kubarski';
+$vanus = 19;
+$kaal = 54.5;
+
+//var_dump($eesnimi); //muutuja sisu test vaatamine
+?>
