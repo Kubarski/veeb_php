82378261d1434197a5162471d3c881b6ab35caaa
Kubarski <ki.tax@hotmail.com>
2019-02-13 15:15:09 +0200

    paaris ja paaritu värvidega täiendatud Div'iga

--- PHP_alused/Condition/index.php
+++ PHP_alused/Condition/index.php
@@ -1,3 +1,19 @@
+<style>
+    div {
+        margin: auto;
+        width: 100px;
+        height: 100px;
+        font-size: 75px;
+        padding-top: 20px;
+        text-align: center;
+    }
+    .paaris{
+        background: red;
+    }
+    .paaritu{
+        background: green;
+    }
+</style>
 <?php
 /**
  * Created by PhpStorm.
