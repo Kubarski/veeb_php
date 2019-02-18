6f440bf214ed19ae7076b1aa6fc72c99a34b0e3f
Kubarski <ki.tax@hotmail.com>
2019-02-18 14:26:27 +0200

    Numbrid kuvatud ühel real

---
+++ Operations/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katriin
+ * Date: 18.02.2019
+ * Time: 10:32
+ */
+//aritmeetilised operaatorid
+$x = 63;
+$y = 9;
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+echo "Vastused: $x + $y = $liitm, $x - $y = $lahut, $x * $y = $korru, $x / $y = $jagam,$x % $y = $jaak";
