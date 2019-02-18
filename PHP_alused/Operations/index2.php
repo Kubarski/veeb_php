0a03597999c240905b4121500998bc6f34bbcb41
Kubarski <ki.tax@hotmail.com>
2019-02-18 15:02:43 +0200

    teisendus celsiusest fahrenheiti

---
+++ PHP_alused/Operations/index2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katriin
+ * Date: 18.02.2019
+ * Time: 14:54
+ */
+//ringi ümbermõõdu ja pindala arvutamine
+$r = 3;
+    $c = 2 * 3.1415 * $r;
+    $s = 3.1415 * $r * $r;
+    echo 'Sellise ringi ümbermõõt on: '.$c.'<br>';
+    echo 'Sellise ringi pindala on: '.$s.'<br>';
+?>
