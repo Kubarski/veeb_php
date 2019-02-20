8f6dbe334756e11bfa601f31cefb598990c6e198
Kubarski <ki.tax@hotmail.com>
2019-02-20 14:40:59 +0200

    lehepais

--- PHP_alused/Functions/index.php
+++ PHP_alused/Functions/index.php
@@ -1,24 +1,5 @@
 <?php
-//defineerime funktsiooni nimega paarsuskontroll
-
-function kaspaaris ($arv){
-    if($arv %2 == 0) {
-        $kontroll = true;
-    } else {
-        $kontroll = false;
-    }
-return $kontroll;
-}
-function kirjeldus($paaris) {
-    if ($paaris == true) {
-        echo ' on paaris';
-    } else {
-        echo ' on paaritu';
-    }
-    echo '<br>';
-}
-//kutsume funktsiooni tööle
-for ($arv=0; $arv <=10; $arv ++){
-    echo $arv;
-    kirjeldus(kaspaaris($arv));
-}
+require_once 'fnc.php';
+lehePais();
+tabel(15);
+?>
