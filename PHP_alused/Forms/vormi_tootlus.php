bc101602d0ccca1e21abd6c5c129f06e247ee45f
Kubarski <ki.tax@hotmail.com>
2019-02-25 12:47:27 +0200

    Vormitöötlus1

--- PHP_alused/Forms/vormi_tootlus.php
+++ PHP_alused/Forms/vormi_tootlus.php
@@ -5,11 +5,15 @@
  * Date: 25.02.2019
  * Time: 11:59
  */
+foreach ($_GET as $nimetus => $vaartus){
+    if ($_GET[$nimetus] == 0){
+        header('Location: index.html');
+    }
 
+}
 extract($_GET);
 
 /*$nimi = $_GET['nimi'];
 $parool = $_GET['parool']*/
-
-echo 'Tere' .$Nimi. '!<br>';
-echo 'Sinu parooliks on ' .$parool;
+    echo 'Tere ' . $nimi . '!<br>';
+    echo 'Sinu parooliks on ' . $parool;
