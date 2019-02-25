90f05a67a48ac649faae34c529d37b2959c7615f
Kubarski <ki.tax@hotmail.com>
2019-02-25 12:19:48 +0200

    Vormitöötlus

---
+++ PHP_alused/Forms/vormi_tootlus.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katriin
+ * Date: 25.02.2019
+ * Time: 11:59
+ */
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+echo '<hr>';
+
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+echo '<hr>';
+
+echo '<pre>';
+print_r($_REQUEST);
+echo '</pre>';
+echo '<hr>';
