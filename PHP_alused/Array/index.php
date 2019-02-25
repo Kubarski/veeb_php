9d13e5b699d20981bdfbb0bba0dd13b0f28535bd
Kubarski <ki.tax@hotmail.com>
2019-02-25 10:39:45 +0200

    Massiivi loomine ja kontrollimine1

--- PHP_alused/Array/index.php
+++ PHP_alused/Array/index.php
@@ -6,51 +6,25 @@
  * Time: 14:58
  */
 
-$raamat1 = array (
+$raamatud = array ( array(
     'nimi' => 'Elus',
     'hind' => 16.95,
     'autor' => 'Jaana Maling',
     'keel' => 'eesti',
     'zanr' =>'Biograafiad ja memuaarid'
-);
-$raamat2 = array (
+),
+array (
         'nimi' => 'Ma abiellusin sinuga',
         'hind' => 4.00,
         'autor' => 'Walter Trobisch',
         'keel' => 'inglise',
         'zanr' =>'Eneseabi'
-    );
-
-$raamat3 = array (
+    ),
+    array (
      'nimi' => 'Nimed marmortahvlil',
      'hind' => 17.60,
      'autor' => 'Albert Kivikas',
      'keel' => 'eesti',
      'zanr' =>'Ilukirjandus'
-);
-    echo '<pre>';
-    print_r($raamat1);
-    echo '</pre>';
-
-    echo '<pre>';
-    print_r($raamat2);
-    echo '</pre>';
-
-    echo '<pre>';
-    print_r($raamat3);
-    echo '</pre>';
-
-foreach ($raamat1 as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-foreach ($raamat2 as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-foreach ($raamat3 as $nimi=>$vaartus){
-    echo $nimi.' - '.$vaartus.'<br>';
-}
-echo '<hr>';
-
+));
 
