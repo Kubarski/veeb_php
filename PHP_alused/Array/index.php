c1dede3269824c3db1d4b483f54158bc3831a03f
Kubarski <ki.tax@hotmail.com>
2019-02-25 11:03:19 +0200

    Massiivi tabeli loomine

--- PHP_alused/Array/index.php
+++ PHP_alused/Array/index.php
@@ -27,4 +27,34 @@ array (
      'keel' => 'eesti',
      'zanr' =>'Ilukirjandus'
 ));
+function tabeliPais($andmed){
+    echo '<thead>';
+    echo '<tr>';
+    foreach ($andmed as $element){
+        echo '<th>'.$element.'</th>';
+    }
+    echo '</tr>';
+    echo '</thead>';
+}
+function tabeliRida($andmed){
+    echo '<tr>';
+    foreach ($andmed as $elemendiNimetus => $elemendiVaartus){
+        echo '<td>'.$elemendiVaartus.'</td>';
+    }
+    echo '</tr>';
+}
+function tabel($andmed){
+    echo '<table>';
+    tabeliPais(array_keys($andmed[0]));
+    echo '<tbody>';
+    foreach ($andmed as $element){
+        tabeliRida($element);
+    }
+    echo '</tbody>';
+    echo '</table>';
+}
+tabel($raamatud);
+
+
+
 
