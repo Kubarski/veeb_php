fa961ee82159a54924cbde4e4858315062663029
Kubarski <ki.tax@hotmail.com>
2019-03-07 09:28:07 +0200

    loodud päringu funktsioon DB

--- PHP_alused/DB/index.php
+++ PHP_alused/DB/index.php
@@ -10,6 +10,15 @@ function dbconnect($h, $u, $p, $db){
     return $connect;
 }
 
+function query ($conn, $sql){
+    $result = mysqli_query($conn, $sql);
+    if ($result == false){
+        echo 'Probleem päringuga: <b>'.$sql.'</b><br>';
+        echo mysqli_error($conn).'<br>';
+        echo mysqli_errno($conn).'<br>';
+    }
+    return $result;
+}
 $connectIKT = dbconnect(HOST, USER, PASS, DB );
-
-
+$sql = 'SELECT NOW()';
+$sqlResult = query($connectIKT, $sql);
