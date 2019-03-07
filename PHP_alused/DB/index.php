a8130044d087161243c754096c89b8ab43b14f9e
Kubarski <ki.tax@hotmail.com>
2019-03-07 09:03:44 +0200

    DB ühendus serveriga olemas

---
+++ PHP_alused/DB/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'db_conf.php';
+
+function dbconnect($h, $u, $p, $db){
+    $connect = mysqli_connect($h, $u, $p, $db);
+    if($connect == false){
+        echo 'Probleem andmebaasi ühendamisega<br>';
+        exit;
+    }
+    return $connect;
+}
+
+$connectIKT = dbconnect(HOST, USER, PASS, DB );
+
+
