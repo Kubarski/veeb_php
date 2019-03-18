3dc52f430bf00943d142eeb98d25278fb090feec
Kubarski <ki.tax@hotmail.com>
2019-03-18 14:23:14 +0200

    oop - lisatud kasutaja sisse/välja logimine süsteemis - andmebaasi tö…
    …ötluse kaudu ja sessiooni töötluse kaudu

--- OOP_PHP/nav.php
+++ OOP_PHP/nav.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Katriin
- * Date: 18.03.2019
- * Time: 13:54
- */
 $nav = new Template('nav.nav');
 $item = new Template('nav.item');
 $item->set('name', 'Main Page');
@@ -15,8 +9,16 @@ $item->set('name', 'Contact');
 $link = $http->getLink(array('controller' => 'contact'));
 $item->set('link', $link);
 $nav->add('items', $item->parse());
-$item->set('name', 'Log In');
-$link = $http->getLink(array('controller' => 'login'));
-$item->set('link', $link);
-$nav->add('items', $item->parse());
+if(USER_ID == ROLE_NONE)
+{
+    $item->set('name', 'Log In');
+    $link = $http->getLink(array('controller' => 'login'));
+    $item->set('link', $link);
+    $nav->add('items', $item->parse());
+} else {
+    $item->set('name', 'Log Out');
+    $link = $http->getLink(array('controller' => 'logout'));
+    $item->set('link', $link);
+    $nav->add('items', $item->parse());
+}
 $mainContent->set('nav', $nav->parse());
