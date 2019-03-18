7d3f12ceba1e7d9a21d83efec5bdc096a406d478
Kubarski <ki.tax@hotmail.com>
2019-03-18 14:54:04 +0200

    oop- lisatud pealehele navigatsiooni url-i kontrollerid ja muudetud vaikimisi kontroller

---
+++ OOP_PHP/CONTROLLERS/login_do.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katriin
+ * Date: 18.03.2019
+ * Time: 14:53
+ */
--- OOP_PHP/CONTROLLERS/main.php
+++ OOP_PHP/CONTROLLERS/main.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Katriin
- * Date: 18.03.2019
- * Time: 13:31
- */
 $mainContent->set('content', 'Main Page Content');
