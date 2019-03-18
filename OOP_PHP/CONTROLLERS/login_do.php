1ebce1e0c7666963b9071d6ed1e18c6b5373b6b5
Kubarski <ki.tax@hotmail.com>
2019-03-18 14:55:12 +0200

    oop - lisatud kasutaja sisse/välja logimine süsteemis - andmebaasi tö…
    …ötluse kaudu ja sessiooni töötluse kaudu

--- OOP_PHP/CONTROLLERS/login_do.php
+++ OOP_PHP/CONTROLLERS/login_do.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Katriin
- * Date: 18.03.2019
- * Time: 14:53
- */
+$username = $http->get('username');
+$password = $http->get('password');
+$sql = 'SELECT * FROM user WHERE username='.fixDb($username).' AND password='.fixDb(md5($password)).' AND is_active=1';
+$result = $db->getData($sql);
+if($result == false){
+    $link = $http->getLink(array('controller' => 'login'));
+    $http->redirect($link);
+} else {
+    $sess->createSession($result[0]);
+//    echo '<pre>';
+//    print_r($http);
+    $http->redirect();
+}
